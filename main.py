8daca7e5eeefc8da428e55a1264dad34f68e8abb
Pasha Tuz <90028614+PashaTuz@users.noreply.github.com>
2021-09-20 12:43:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-r = int(input('Ведіьб радіус - '))
-c = 2 * r * 3.14159
-s = 3.14159* r ** 2
-v = 3 / 4 * 3.14159 * r ** 3
-print("Довжина круга = {}, Площа круга = {}, Об'єм кулі = {}".format(c, s, v))
+
+a, b = 1000, 7.77
+while True:
+    if a > 0:
+        print(f'{a} - {b}= { a - b}')
+        a-= b
+    else:break
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
