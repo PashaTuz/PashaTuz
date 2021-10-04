b6b72b76b7b7faa05fe00ec58da5f48db465b968
Pasha Tuz <90028614+PashaTuz@users.noreply.github.com>
2021-10-04 19:55:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,53 +11,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_hi('PyCharm')
 
-    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-    x = 0
-    print(x)
-    x = float(0)
-    print(x)
-    a = 101
-    print(a)
-    x = float(1000)
-    print(x)
-    import math
-
-    x = 1.0 * (10 ** 3)
-    print(x)
-    a = 2 * 5
-    print(x)
-    b = 5 * (-2)
-    print(x)
-    c = 222 * 0
-    print(a, b, c)
-    q = 15
-    w = 13
-    e = 2
-    r = int(((q + w) * e - (e + w) - 1) / e)
-    print(r)
-    print("Hello student!")
-    print("можна з комою")
-    a = float(input("Первое число ="))
-    oper = input("Введите операцыю =")
-    b = float(input("Второе число ="))
-    if oper == '+':
-        print(a + b)
-    elif oper == '-':
-        print(a - b)
-    elif oper == '*':
-        print(a * b)
-    elif oper == '/':
-        print(a / b)
-    print(" Тільки цілі")
-a = int(input("Первое число ="))
-oper = input("Введите операцыю =")
-b = int(input("Второе число ="))
-if oper == '+':
-    print(a + b)
-elif oper == '-':
-    print(a - b)
-elif oper == '*':
-    print(a * b)
-elif oper == '/':
-    print(a / b)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = int(input("Ведіть номер кредитної карти - "))
+k = a % 10000
+print(k)
