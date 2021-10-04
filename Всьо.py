8d5bb19561b0841891cef9084f3755e17d78accc
Pasha Tuz <90028614+PashaTuz@users.noreply.github.com>
2021-10-04 19:57:31 +0300

    Add files via upload

---
+++ Всьо.py
@@ -0,0 +1,63 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    x = 0
+    print(x)
+    x = float(0)
+    print(x)
+    a = 101
+    print(a)
+    x = float(1000)
+    print(x)
+    import math
+
+    x = 1.0 * (10 ** 3)
+    print(x)
+    a = 2 * 5
+    print(x)
+    b = 5 * (-2)
+    print(x)
+    c = 222 * 0
+    print(a, b, c)
+    q = 15
+    w = 13
+    e = 2
+    r = int(((q + w) * e - (e + w) - 1) / e)
+    print(r)
+    print("Hello student!")
+    print("можна з комою")
+    a = float(input("Первое число ="))
+    oper = input("Введите операцыю =")
+    b = float(input("Второе число ="))
+    if oper == '+':
+        print(a + b)
+    elif oper == '-':
+        print(a - b)
+    elif oper == '*':
+        print(a * b)
+    elif oper == '/':
+        print(a / b)
+    print(" Тільки цілі")
+a = int(input("Первое число ="))
+oper = input("Введите операцыю =")
+b = int(input("Второе число ="))
+if oper == '+':
+    print(a + b)
+elif oper == '-':
+    print(a - b)
+elif oper == '*':
+    print(a * b)
+elif oper == '/':
+    print(a / b)
